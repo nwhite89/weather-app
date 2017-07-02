479bbfde8d6b118fb9209fc0b5793313bacfb35f
Nick White <nick.white@scoota.com>
2017-07-02 18:18:46 +0100

    Display location in header.

--- src/app/weather-list/weather-list.component.ts
+++ src/app/weather-list/weather-list.component.ts
@@ -18,6 +18,8 @@ export class WeatherListComponent implements OnInit {
 
     ngOnInit() {
         const now = new Date();
+        // Get a list of timestamps for today and the next 5 days this will then allow us to group easier within the
+        // template to know whether the weather is for the same day or the next day.
         for (let index = 0; index < this.days; index++) {
             const startOfDay: any = new Date(now.getFullYear(), now.getMonth(), now.getDate() + index),
                 timestamp = startOfDay / 1000;
@@ -32,6 +34,8 @@ export class WeatherListComponent implements OnInit {
     fetchData() {
         this.openmapWeatherService.getLocationData(this.value).subscribe((data: any) => {
 
+            // Set a timestamp to allow for using the Angular Date pipe as OpenMap returns not as a full timestamp
+            // that is usuable in JS Date format
             data.list.map((val: any) => {
                 val.unix = parseInt(val.dt + '000', 10);
             });
@@ -41,6 +45,7 @@ export class WeatherListComponent implements OnInit {
     }
 
     onEnter(value: string) {
+        // If a user doesn't submit a location default back to London
         if (value) {
             this.value = value;
         } else {
